39488f3fd2272a7447dce53ac1d01935b85fda04
AlexByte <alex.g.cortes.g@gmail.com>
2023-11-12 23:43:41 -0600

    add express

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
-import fs from 'fs';
-import http from 'http';
+// import fs from 'fs';
+// import http from 'http';
 
-const server = http.createServer((req, res) => {
-    console.log (req.url);
+import { Server } from "./presentation/server";
 
-    if (req.url === '/') {
-        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(htmlFile);
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end();
-    }
+// const server = http.createServer((req, res) => {
+//     console.log (req.url);
 
-});
+//     if (req.url === '/') {
+//         const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.end(htmlFile);
+//     } else {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         res.end();
+//     }
 
-server.listen(8080, () => {
-    console.log('Server running on port 8080');
-})
+// });
+
+// server.listen(8080, () => {
+//     console.log('Server running on port 8080');
+// })
+
+(async () => {
+    main();
+})();
+
+function main() {
+    const server = new Server();
+
+    server.start();
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export class Server {
+
+    private app = express();
+
+    async start() {
+
+        this.app.use( express.static('public'));
+
+        this.app.listen(3000, () => {
+            console.log('server running on port 3000');
+        });
+    }
+
+}
