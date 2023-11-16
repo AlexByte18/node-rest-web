6c6ec55ef1b07ad1351118f31366acdaf54b7b17
AlexByte <alex.g.cortes.g@gmail.com>
2023-11-15 22:08:28 -0600

    add todos modules

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,47 @@
+import { Router } from 'express';
 import express from 'express';
+import path from 'path';
+import { envs } from '../config/envs';
+
+
+interface Options {
+    port: number,
+    public_path: string,
+    routes: Router
+}
 
 export class Server {
 
     private app = express();
+    private readonly port: number;
+    private readonly publicPath: string;
+    private readonly routes: Router;
+
+    constructor(options: Options) {
+        const {port, public_path = 'public', routes} = options;
+        this.port = port;
+        this.publicPath = public_path;
+        this.routes = routes;
+    }
 
     async start() {
 
-        this.app.use( express.static('public'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
 
-        this.app.listen(3000, () => {
+        this.app.use( express.static(envs.PUBLIC_PATH));
+
+        this.app.use(this.routes);
+
+
+        this.app.get('*', (req, res) => {
+            console.log(req.url);
+            
+            const indexPath = path.join(__dirname + '../../../public/index.html');
+            res.sendFile(indexPath);
+        });
+
+        this.app.listen(envs.PORT, () => {
             console.log('server running on port 3000');
         });
     }
